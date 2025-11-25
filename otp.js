ecc7b5690a42100fc2f6cf2a1c24d53434d8bfd8
Sakshi-2809 <sakshiydb@gmail.com>
2025-11-25 15:35:14 +0530

    Add files via upload

---
+++ otp.js
@@ -0,0 +1,24 @@
+
+function getotp(){0.
+    var otp=Math.random()*10000;
+    ot=Math.floor(otp);
+    // alert(ot);
+    document.writeln("Your Otp Is"+" ");
+   
+    document.writeln(ot);
+    document.body.style.backgroundColor="black";
+    document.body.style.textShadow="0 0 50px aqua";
+    document.body.style.color="aqua";
+    document.body.style.height="100px";
+    document.body.style.width="500px";
+    document.body.style.fontSize="60px";
+    document.body.style.fontFamily="serif";
+    document.body.style.fontStyle="italic";
+    document.body.style.textAlign="center";
+    document.body.style.margin="auto";
+    document.body.style.padding="60px";
+    
+
+}
+
+
